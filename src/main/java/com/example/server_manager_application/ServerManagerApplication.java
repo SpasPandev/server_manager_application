ba121c36368eb9fe3c12bd377eeb139026884b75
SpasPandev <sppan@abv.bg>
2024-09-13 13:29:10 +0300

    Initial commit

---
+++ src/main/java/com/example/server_manager_application/ServerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.server_manager_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/server_manager_application/ServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server_manager_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
